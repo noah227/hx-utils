3485859aa5d49bfd7fa0c186c0667bbd1074269f
noah227 <noah227@foxmail.com>
2024-11-08 13:33:33 +0800

    chore: 优化类型文件生成函数

--- build/index.js
+++ build/index.js
@@ -4,7 +4,7 @@ const generateWatchConfigurationKeys = () => {
     const hxSettings = require("json5").parse(fs.readFileSync(path.resolve(__dirname, "./hx.settings.json5"), {encoding: 'utf8'}))
     fs.writeFileSync(
         path.resolve(__dirname, "../src/index.event.types.ts"),
-        `/** Auto Generated - ${new Date().toLocaleString()} */\nexport type TWatchSection = \n\t${Object.keys(hxSettings).map(k => `"${k}"`).join(" | \n\t")}`
+        `/** Auto Generated - ${new Date().toLocaleString()} */\nexport type TWatchSection =\n\t${Object.keys(hxSettings).map(k => `"${k}"`).join(" |\n\t")}`
     )
 }
 
